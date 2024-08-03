83ec18252ea5060eb60f711d3026b0ed8038eb26
S1mon <jaforsi.kozak@gmail.com>
2024-08-03 18:29:52 +0200

    added flags for sorting listing by line count

--- main.py
+++ main.py
@@ -16,11 +16,14 @@ parser = argparse.ArgumentParser(
 parser.add_argument('path', nargs='?', default=".")
 parser.add_argument('-l', '--nolist', action="store_true", help="don't list individual file")
 parser.add_argument('-t', '--type', help="filter file types", required=False, dest="type", nargs='*')
-parser.add_argument('-v', '--verbose', action="store_true")
-parser.add_argument('-d', '--hidden' , action="store_true", help="include hidden directories in scan")
+parser.add_argument('-v', '--verbose'      , action="store_true")
+parser.add_argument('-d', '--hidden'       , action="store_true", help="include hidden directories in scan")
+
+sortFlags = parser.add_mutually_exclusive_group()
+sortFlags.add_argument('-s', '--sort'         , dest='sort' , action="store_true", help="sort path listing by line count, small to large")
+sortFlags.add_argument('-S', '--sort-reverse' , dest='rsort', action="store_true", help="sort path listing by line count, large to small")
 
 listingTypeFlags = parser.add_mutually_exclusive_group()
-
 listingTypeFlags.add_argument('-a', '--abs', action="store_true", help="show absolute of file when listing")
 listingTypeFlags.add_argument('-r', '--rel', action="store_true", help="show relative of file when listing")
 
@@ -60,6 +63,14 @@ for (root, dirs, namesRaw) in os.walk(path, topdown=True):
     pool += files
     
 
+@dataclasses.dataclass
+class cEntry:
+    count : int
+    path  : str
+
+
+listing = []
+
 #iterate and count
 total = 0
 for file in pool:
@@ -72,15 +83,24 @@ for file in pool:
             
             if not args.nolist:
                 
-                if   args.abs: listing = file
-                elif args.rel: listing = os.path.relpath(file, path)
-                else         : listing = name
+                if   args.abs: listingPath = file
+                elif args.rel: listingPath = os.path.relpath(file, path)
+                else         : listingPath = name
                 
-                print(f"{count: <10} {listing}")
+                listing.append(cEntry(count=count, path=listingPath))
     
     except UnicodeDecodeError:
         if args.verbose:
             print(f"Unable to decode {name}")
+            
+            
+#sort
+if args.sort : listing.sort(key=lambda x: x.count, reverse=False)
+if args.rsort: listing.sort(key=lambda x: x.count, reverse=True)
+
+#list
+for entry in listing:            
+    print(f"{entry.count: <10} {entry.path}")
             
 print(f"\n{total: <10} total")
 
